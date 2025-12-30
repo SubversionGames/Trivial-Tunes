b9be8e938a948ee5424915b0b35abdc208b809d8
SubversionGames <subversionmole@gmail.com>
2025-12-30 02:26:20 -0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCaVkjG2zXHVnY1NXpwsDXPvg_uCNrDsJ8",
+  authDomain: "trivial-tunes.firebaseapp.com",
+  projectId: "trivial-tunes",
+  storageBucket: "trivial-tunes.firebasestorage.app",
+  messagingSenderId: "320428434618",
+  appId: "1:320428434618:web:e8c09a5a7a8e47a34fce83"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+const app = initializeApp(firebaseConfig);
